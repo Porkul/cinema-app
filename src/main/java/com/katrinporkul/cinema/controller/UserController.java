f4860e6dc0d95c9b6b9288783f7b332a2ce3149e
Katrin Porkul <86647070+Porkul@users.noreply.github.com>
2023-09-01 12:50:15 +0300

    Update UserController.java

--- src/main/java/com/katrinporkul/cinema/controller/UserController.java
+++ src/main/java/com/katrinporkul/cinema/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.katrinporkul.cinema.controller;
 
-//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin("*")
 public class UserController {
 
     @GetMapping("/")
